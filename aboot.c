948220f6ce26614f791555b1a97f5382b0c8dd95
Andrew Boie <andrew.p.boie@intel.com>
2014-05-21 16:08:50 -0700

    add "oem setvar" command
    Allows the user of an unlocked device to set EFI variables to
    string values, which are up-converted to UTF-16.
    
    Change-Id: I90c13e1defc34ee08d32d9d7b6ada96f8d6619cd
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/192903
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Bergeron, Michael <michael.bergeron@intel.com>
    Reviewed-by: Dattatraya Kulkarni, Anisha <anisha.dattatraya.kulkarni@intel.com>

--- aboot.c
+++ aboot.c
@@ -888,6 +888,49 @@ out:
 	return ret;
 }
 
+static int set_efi_var(int argc, char **argv)
+{
+	int ret;
+	efi_guid_t fastboot_guid = FASTBOOT_GUID;
+	size_t datalen;
+	uint16_t *data, *d_pos;
+	char *o_pos;
+
+	if (argc != 3) {
+		pr_error("incorrect number of parameters");
+		return -1;
+	}
+
+	if (strlen(argv[1]) > 128) {
+		pr_error("pathologically long variable name");
+		return -1;
+	}
+
+	/* up-convert the data to a 16-bit string as that is what EFI generally uses */
+	datalen = (strlen(argv[2]) + 1) * 2;
+	if (datalen > 256) { // value is arbitray but should be more than enough
+		pr_error("pathologically long data string");
+		return -1;
+	}
+	data = xmalloc(datalen);
+	d_pos = data;
+	o_pos = argv[2];
+	while (*o_pos)
+		*d_pos++ = *o_pos++;
+	*d_pos = 0;
+
+	pr_debug("Setting '%s' to value '%s'\n", argv[1], argv[2]);
+	ret = efi_set_variable(fastboot_guid, argv[1],
+			(uint8_t *)data, datalen,
+			EFI_VARIABLE_NON_VOLATILE |
+			EFI_VARIABLE_RUNTIME_ACCESS |
+			EFI_VARIABLE_BOOTSERVICE_ACCESS);
+	if (ret)
+		pr_error("Couldn't set '%s' EFI variable to '%s'\n",
+				argv[1], argv[2]);
+	return ret;
+}
+
 static void publish_from_prop(char *key, char *prop, char *dfl)
 {
 	char val[PROPERTY_VALUE_MAX];
@@ -996,6 +1039,7 @@ void aboot_register_commands(void)
 	aboot_register_flash_cmd("gpt", cmd_flash_gpt);
 	aboot_register_oem_cmd("adbd", start_adbd);
 	aboot_register_oem_cmd("garbage-disk", garbage_disk);
+	aboot_register_oem_cmd("setvar", set_efi_var);
 	register_userfastboot_plugins();
 
 	fastboot_publish("unlocked", xstrdup(is_loader_locked() ? "no" : "yes"));
